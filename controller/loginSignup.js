28560618a0e9cc72c91a0e020e70f87d7ac45358
Amit-Yadav21 <amit21navgurukul.org>
2024-12-23 13:59:41 +0530

    install bcryptjs

--- controller/loginSignup.js
+++ controller/loginSignup.js
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { Register } from '../MongoesSchema/registerSchema.js';
 import { createToken } from '../Midlewere/authentication.js';
 
